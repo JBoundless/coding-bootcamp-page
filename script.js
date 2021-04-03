900a24d37b390f4cc573ca62a241acad85f81b42
JBoundless <39219944+JBoundless@users.noreply.github.com>
2021-04-03 13:06:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function alertFunction() {
   alert("Thanks for signing up for the HMTL Coding bootcamp!  More info to come!")
 }
+//Scroll Reveal
+ScrollReveal().reveal('.grads-work-at');
+ScrollReveal().reveal('.sign-up-form', { delay: 500 });
